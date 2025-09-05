7848308cc6b9ba617fa8a5ce0943cc362a7c395b
ccarlzm <csanz@baniba.com>
2025-09-05 08:24:21 -0400

    Add compcore/compcore/settings.py and config

--- manage.py
+++ manage.py
@@ -2,10 +2,22 @@
 import os
 import sys
 
+
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'compcore.compcore.settings.base')
-    from django.core.management import execute_from_command_line
+    """
+    Punto de entrada para utilidades de línea de comando de Django.
+    """
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "compcore.compcore.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "No se pudo importar Django. ¿Está instalado y disponible en tu "
+            "PYTHONPATH? ¿Activaste tu entorno virtual?"
+        ) from exc
     execute_from_command_line(sys.argv)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
+
